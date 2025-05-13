418df5ba8aafa35ad320acac9a5af8c07ce03a7b
UsmanDesigner <usmanaziz.designer@gmail.com>
2025-05-13 10:54:38 +0500

    - update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Website Builder',
+  description: 'Created in Next',
   generator: 'v0.dev',
 }
 
